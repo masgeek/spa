9a3c25c23624d04d02a965acd34e7ab48532c5e7
Sammy Barasa <barsamms@gmail.com>
2017-07-16 22:31:41 +0300

    Added services

---
+++ api/modules/v1/controllers/OfferedserviceController.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RONIN
+ * Date: 7/16/2017
+ * Time: 8:47 PM
+ */
+
+namespace app\api\modules\v1\controllers;
+
+use app\api\modules\v1\models\OFFERED_SERVICE_MODEL;
+use app\api\modules\v1\models\SERVICE_MODEL;
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\filters\AccessControl;
+use yii\rest\ActiveController;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+use yii\rest\Controller;
+use yii\web\Response;
+
+class OfferedserviceController extends ActiveController
+{
+	/**
+	 * @var object
+	 */
+	public $modelClass = 'app\api\modules\v1\models\OFFERED_SERVICE_MODEL';
+
+	public function behaviors()
+	{
+		$behaviors = parent::behaviors();
+		$behaviors['rateLimiter']['enableRateLimitHeaders'] = false;
+		return $behaviors;
+	}
+
+	public function actionIndex()
+	{
+		return new ActiveDataProvider([
+			'query' => OFFERED_SERVICE_MODEL::findOne(4),
+		]);
+	}
+
+	public function actionAll()
+	{
+
+	}
+}
---
+++ api/modules/v1/models/OFFERED_SERVICE_MODEL.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RONIN
+ * Date: 7/16/2017
+ * Time: 10:30 PM
+ */
+
+namespace app\api\modules\v1\models;
+
+
+use app\models\OfferedServices;
+
+class OFFERED_SERVICE_MODEL extends OfferedServices
+{
+
+}
