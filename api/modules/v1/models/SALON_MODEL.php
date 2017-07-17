a658925cbe33385a17bc8110597b253238d9d230
Sammy Barasa <barsamms@gmail.com>
2017-07-17 20:35:14 +0300

    updates

--- api/modules/v1/models/SALON_MODEL.php
+++ api/modules/v1/models/SALON_MODEL.php
@@ -14,7 +14,7 @@ use yii\helpers\Url;
 use yii\web\Link;
 use yii\web\Linkable;
 
-class SALON_MODEL extends \app\models\Salon implements Linkable, RateLimitInterface
+class SALON_MODEL extends \app\models\Salon
 {
 
 	const SCENARIO_CREATE = 'create';
@@ -38,40 +38,6 @@ class SALON_MODEL extends \app\models\Salon implements Linkable, RateLimitInterf
 	public function fields()
 	{
 		$fields = parent::fields();
-
-		//unset($fields['auth_key'], $fields['password_hash'], $fields['password_reset_token']);
 		return $fields;
-	}
-
-	/*public function extraFields()
-	{
-		return ['profile'];
-	}*/
-	public function getLinks()
-	{
-		return [
-			Link::REL_SELF => Url::to(['salon/view', 'id' => $this->SALON_ID], true),
-			'edit' => Url::to(['salon/view', 'id' => $this->SALON_ID], true),
-			//'profile' => Url::to(['salon/profile/view', 'id' => $this->SALON_ID], true),
-			//'index' => Url::to(['SALON_MODEL'], true),
-		];
-	}
-
-	//Rate limiters
-	public function getRateLimit($request, $action)
-	{
-		return [$this->rateLimit, 1]; // $rateLimit requests per second
-	}
-
-	public function loadAllowance($request, $action)
-	{
-		return [$this->allowance, $this->allowance_updated_at];
-	}
-
-	public function saveAllowance($request, $action, $allowance, $timestamp)
-	{
-		$this->allowance = $allowance;
-		$this->allowance_updated_at = $timestamp;
-		$this->save();
 	}
 }
