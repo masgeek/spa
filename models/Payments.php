11610992ee020aef811436f0f213b56206419e83
Sammy Barasa <barsamms@gmail.com>
2017-07-24 22:56:10 +0300

    models update

--- models/Payments.php
+++ models/Payments.php
@@ -12,6 +12,7 @@ use Yii;
  * @property string $BOOKING_AMOUNT
  * @property string $FINAL_AMOUNT
  * @property string $DATE_PAID
+ * @property string $PAYMENT_REF
  *
  * @property Reservations $rESERVATION
  */
@@ -35,6 +36,7 @@ class Payments extends \yii\db\ActiveRecord
             [['RESERVATION_ID'], 'integer'],
             [['BOOKING_AMOUNT', 'FINAL_AMOUNT'], 'number'],
             [['DATE_PAID'], 'safe'],
+            [['PAYMENT_REF'], 'string', 'max' => 50],
             [['RESERVATION_ID'], 'exist', 'skipOnError' => true, 'targetClass' => Reservations::className(), 'targetAttribute' => ['RESERVATION_ID' => 'RESERVATION_ID']],
         ];
     }
@@ -50,6 +52,7 @@ class Payments extends \yii\db\ActiveRecord
             'BOOKING_AMOUNT' => 'Booking  Amount',
             'FINAL_AMOUNT' => 'Final  Amount',
             'DATE_PAID' => 'Date  Paid',
+            'PAYMENT_REF' => 'Payment  Ref',
         ];
     }
 
