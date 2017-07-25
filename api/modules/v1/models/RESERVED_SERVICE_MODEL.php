b7c9685596a81de5c8e808f59a59ba5fcdbaa84d
Sammy Barasa <sammy.barasa@pristone.net>
2017-07-25 11:13:55 +0300

    Update

--- api/modules/v1/models/RESERVED_SERVICE_MODEL.php
+++ api/modules/v1/models/RESERVED_SERVICE_MODEL.php
@@ -23,38 +23,40 @@ class RESERVED_SERVICE_MODEL extends ReservedServices
 	public function fields()
 	{
 		$fields = parent::fields();
-		$fields['SERVICE_NAME'] = function ($model) {
-			/* @var $model RESERVED_SERVICE_MODEL */
-			return SERVICE_MODEL::findOne($model->OFFERED_SERVICE_ID)->SERVICE_NAME;
-		};
+		if(!$this->isNewRecord) {
+            $fields['SERVICE_NAME'] = function ($model) {
+                /* @var $model RESERVED_SERVICE_MODEL */
+                return SERVICE_MODEL::findOne($model->OFFERED_SERVICE_ID)->SERVICE_NAME;
+            };
 
-		$fields['SALON_NAME'] = function ($model) {
-			/* @var $model RESERVED_SERVICE_MODEL */
-			if ($model->oFFEREDSERVICE != null) {
-				return $model->oFFEREDSERVICE->sALON->SALON_NAME;
-			}
-		};
+            $fields['SALON_NAME'] = function ($model) {
+                /* @var $model RESERVED_SERVICE_MODEL */
+                if ($model->oFFEREDSERVICE != null) {
+                    return $model->oFFEREDSERVICE->sALON->SALON_NAME;
+                }
+            };
 
-		$fields['STAFF'] = function ($model) {
-			/* @var $model RESERVED_SERVICE_MODEL */
-			$staff = 'Not Assigned';
-			if ($model->sTAFF != null) {
-				$staff = $model->sTAFF->STAFF_NAME;
-			}
+            $fields['STAFF'] = function ($model) {
+                /* @var $model RESERVED_SERVICE_MODEL */
+                $staff = 'Not Assigned';
+                if ($model->sTAFF != null) {
+                    $staff = $model->sTAFF->STAFF_NAME;
+                }
 
-			return $staff;
-		};
+                return $staff;
+            };
 
-		$fields['STATUS'] = function ($model) {
-			/* @var $model RESERVED_SERVICE_MODEL */
-			$status = 'Pending';
-			if ($model->sTATUS != null) {
-				$status = $model->sTATUS->STATUS_NAME;
-			}
-			return $status;
-		};
-		unset($fields['STAFF_ID']);
-		unset($fields['STATUS_ID']);
+            $fields['STATUS'] = function ($model) {
+                /* @var $model RESERVED_SERVICE_MODEL */
+                $status = 'Pending';
+                if ($model->sTATUS != null) {
+                    $status = $model->sTATUS->STATUS_NAME;
+                }
+                return $status;
+            };
+            unset($fields['STAFF_ID']);
+            unset($fields['STATUS_ID']);
+        }
 		return $fields;
 	}
 }
