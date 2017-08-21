4eb80cf94a512667fef6f35bc736505cd56dd108
Sammy Barasa <barsamms@gmail.com>
2017-08-21 15:50:08 +0300

    updates

--- controllers/ReportsController.php
+++ controllers/ReportsController.php
@@ -2,8 +2,83 @@
 
 namespace app\controllers;
 
+use app\models\VwAllReservations;
+
 class ReportsController extends \yii\web\Controller
 {
+	public function actionIndex(){
+
+
+		$fromDate ='01/01/2017'; //$request->input('from_date');
+		$toDate = '01/01/2018';//$request->input('to_date');
+		$sortBy = 'RESERVATION_DATE';//$request->input('sort_by');
+
+		// Report title
+		$title = 'Registered User Report';
+
+		// For displaying filters description on header
+		$meta = [
+			'Registered on' => $fromDate . ' To ' . $toDate,
+			'Sort By' => $sortBy
+		];
+
+		$queryBuilder = VwAllReservations::select(['SALON_NAME', 'SERVICE_NAME', 'RESERVATION_DATE'])
+			->whereBetween('RESERVATION_DATE', [$fromDate, $toDate])
+			->orderBy($sortBy);
+
+		// Set Column to be displayed
+		$columns = [
+			'Name' => 'SALON_NAME',
+			'RESERVATION_DATE', // if no column_name specified, this will automatically seach for snake_case of column name (will be registered_at) column from query result
+			'Total Balance' => 'BALANCE',
+			'Status' => function($result) { // You can do if statement or any action do you want inside this closure
+				return ($result->BALANCE > 100000) ? 'Rich Man' : 'Normal Guy';
+			}
+		];
+
+		/*
+	Generate Report with flexibility to manipulate column class even manipulate column value (using Carbon, etc).
+
+	- of()         : Init the title, meta (filters description to show), query, column (to be shown)
+	- editColumn() : To Change column class or manipulate its data for displaying to report
+	- editColumns(): Mass edit column
+	- showTotal()  : Used to sum all value on specified column on the last table (except using groupBy method). 'point' is a type for displaying total with a thousand separator
+	- groupBy()    : Show total of value on specific group. Used with showTotal() enabled.
+	- limit()      : Limit record to be showed
+	- make()       : Will producing DomPDF / SnappyPdf instance so you could do any other DomPDF / snappyPdf method such as stream() or download()
+*/
+		$t =  PdfReport::of($title, $meta, $queryBuilder, $columns)
+			->editColumn('Registered At', [
+				'displayAs' => function($result) {
+					return $result->RESERVATION_DATE;//->format('d M Y');
+				}
+			])
+			->editColumn('Total Balance', [
+				'displayAs' => function($result) {
+					return $result->BALANCE;
+				}
+			])
+			->editColumns(['Total Balance', 'Status'], [
+				'class' => 'right bold'
+			])
+			->showTotal([
+				'Total Balance' => 'point'
+			])
+			->limit(20)
+			->stream(); // or download('filename here..') to download pdf
+
+		return 1;
+	}
+	public function actionIndex2()
+	{
+		$data = VwAllReservations::find()->all();
+
+		var_dump($data);
+
+		return 1;
+		return $this->render('index');
+	}
+
     public function actionAllReservations()
     {
         return $this->render('all-reservations');
@@ -12,11 +87,6 @@ class ReportsController extends \yii\web\Controller
     public function actionCustomers()
     {
         return $this->render('customers');
-    }
-
-    public function actionIndex()
-    {
-        return $this->render('index');
     }
 
     public function actionPayments()
