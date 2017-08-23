748ff717e5de06f1b9d0cc2f3120a18f88c6ebca
Sammy Barasa <sammy.barasa@pristone.net>
2017-08-23 11:09:02 +0300

    update

--- views/service/view.php
+++ views/service/view.php
@@ -28,7 +28,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'SERVICE_ID',
+            //'SERVICE_ID',
             'SERVICE_NAME',
             'DESCRIPTION:ntext',
         ],
