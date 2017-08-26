c3074a4f0a1e5cd77e392d3bd120edddb48d9def
Sammy Barasa <barsamms@gmail.com>
2017-08-26 17:39:45 +0300

    update

--- api/modules/v1/controllers/ReservationController.php
+++ api/modules/v1/controllers/ReservationController.php
@@ -273,12 +273,10 @@ class ReservationController extends ActiveController
 		$reservation_id = $request->RESERVATION_ID;
 
 		$model = RESERVATION_MODEL::findOne($reservation_id);
-		$model->STATUS_ID = 1;  //flag as confirmed
-		//if (!$model->save() && !$model->validate()) {
-		//	return $model;
-		//} else {
-			$model = ['message' => 'not saved'];
-		//}
+		$model->STATUS_ID = 199;  //flag as confirmed
+		if (!$model->save() && !$model->validate()) {
+			$model = ['message' => 'Unable to confirm reservation please contact the Adminstrator'];
+		}
 		return $model;
 	}
 
