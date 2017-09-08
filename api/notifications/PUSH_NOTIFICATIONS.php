4055a600ab85ab1179e207ef8cec2bd60164ab4c
Sammy Barasa <sammy.barasa@pristone.net>
2017-09-08 12:16:51 +0300

    Updates on push notification

--- api/notifications/PUSH_NOTIFICATIONS.php
+++ api/notifications/PUSH_NOTIFICATIONS.php
@@ -8,23 +8,29 @@
 
 namespace app\api\notifications;
 
+use paragraph1\phpFCM\Message;
 use paragraph1\phpFCM\Recipient\Device;
+use understeam\fcm\Client;
 use Yii;
 
 class PUSH_NOTIFICATIONS
 {
     public function NotifyUser($msgTitle, $msgBody, $deviceToken)
     {
+        /* @var $push Client*/
         if ($deviceToken == null) {
             return false;
         }
 
-        $note = Yii::$app->fcm->createNotification($msgTitle, $msgBody);
+        $push = Yii::$app->fcm;
+
+        $note =$push->createNotification($msgTitle, $msgBody);
         $note->setIcon('notification_icon_resource_name')
             ->setColor('#ff4081')
             ->setBadge(1);
 
-        $message = Yii::$app->fcm->createMessage();
+
+        $message = $push->createMessage();
         $message->addRecipient(new Device($deviceToken));
         // $message->addRecipient(new Topic('SPA'));
         $message->setNotification($note)
