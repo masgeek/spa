92e0cbf7cb8fbc5f2c5f36a742751a84f20dcef3
Sammy Barasa <barsamms@gmail.com>
2017-09-11 11:11:58 +0300

    Readded service name

--- models/VwMyReservations.php
+++ models/VwMyReservations.php
@@ -19,6 +19,7 @@ use Yii;
  * @property int $RESERVER_ID
  * @property string $COMMENTS
  * @property int $SALON_ID
+ * @property string $RESERVATION_TIME
  */
 class VwMyReservations extends \yii\db\ActiveRecord
 {
@@ -37,8 +38,8 @@ class VwMyReservations extends \yii\db\ActiveRecord
     {
         return [
             [['RESERVATION_ID', 'STATUS_ID', 'OWNER_ID', 'RESERVER_ID', 'SALON_ID'], 'integer'],
-            [['RESERVATION_DATE', 'TOTAL_COST', 'ACCOUNT_REF', 'SALON_NAME', 'SURNAME', 'RESERVER_ID'], 'required'],
-            [['RESERVATION_DATE'], 'safe'],
+            [['RESERVATION_DATE', 'TOTAL_COST', 'ACCOUNT_REF', 'SALON_NAME', 'SURNAME', 'RESERVER_ID', 'RESERVATION_TIME'], 'required'],
+            [['RESERVATION_DATE', 'RESERVATION_TIME'], 'safe'],
             [['TOTAL_COST', 'BOOKING_AMOUNT'], 'number'],
             [['COMMENTS'], 'string'],
             [['ACCOUNT_REF'], 'string', 'max' => 50],
@@ -65,6 +66,7 @@ class VwMyReservations extends \yii\db\ActiveRecord
             'RESERVER_ID' => 'Reserver  ID',
             'COMMENTS' => 'Comments',
             'SALON_ID' => 'Salon  ID',
+            'RESERVATION_TIME' => 'Reservation  Time',
         ];
     }
 }
