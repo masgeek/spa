33b99ff51ff4bc08fd25fddef7514bce500e5521
Sammy Barasa <barsamms@gmail.com>
2017-09-22 20:49:08 +0300

    Removed date filter

--- api/modules/v1/controllers/ReservationController.php
+++ api/modules/v1/controllers/ReservationController.php
@@ -219,7 +219,7 @@ class ReservationController extends ActiveController
 		}
 		$query = MY_RESERVATIONS::find()
 			->where(['USER_ID' => $id])
-			->andWhere(['>=', 'RESERVATION_DATE', $currentDate])
+			//->andWhere(['>=', 'RESERVATION_DATE', $currentDate])
 			->orderBy(['RESERVATION_DATE' => SORT_DESC]);
 
 		$provider = new ActiveDataProvider([
