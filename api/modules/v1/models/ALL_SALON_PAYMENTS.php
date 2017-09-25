22cffb6bee665e98ee58ff3d12c05249649fcb9a
Sammy Barasa <barsamms@gmail.com>
2017-09-25 22:33:03 +0300

    Aded endpoint for getting payments for a certain salon

--- api/modules/v1/models/ALL_SALON_PAYMENTS.php
+++ api/modules/v1/models/ALL_SALON_PAYMENTS.php
@@ -9,6 +9,7 @@
 namespace app\api\modules\v1\models;
 
 
+use app\model_extended\MY_PAYMENTS_MODEL;
 use app\model_extended\MY_RESERVATIONS_VIEW;
 
 class ALL_SALON_PAYMENTS extends MY_RESERVATIONS_VIEW
@@ -16,14 +17,15 @@ class ALL_SALON_PAYMENTS extends MY_RESERVATIONS_VIEW
 	public function fields()
 	{
 		$fields = parent::fields();
-
-		$fields['SERVICES'] = function ($model) {
-			$query = RESERVED_SERVICE_MODEL::find()
+		$fields['PAYMENTS'] = function ($model) {
+			$query = MY_PAYMENTS_MODEL::find()
 				->where(['RESERVATION_ID' => $model->RESERVATION_ID])
 				->all();
 
 			return $query;
 		};
+
+		unset($fields['SERVICES']);
 		return $fields;
 	}
 }
